d4ec9bc9066c49249faa18197ec97b3371a5c5d8
Mateus Oliveira <mateusoli.viana@gmail.com>
2019-02-16 15:35:39 -0200

    FEATURE - create method sacar

--- prjBanco/src/Model/ContaCorrente.java
+++ prjBanco/src/Model/ContaCorrente.java
@@ -1,8 +1,12 @@
 package Model;
 
-public class ContaCorrente {
+public class ContaCorrente extends Pessoa {
 
     private double saldo;
+
+    public ContaCorrente(String nome, String sobrenome, int cpf) {
+        super(nome, sobrenome, cpf);
+    }
 
     public double getSaldo() {
         return saldo;
@@ -12,4 +16,34 @@ public class ContaCorrente {
         this.saldo = saldo;
     }
 
+    public String depositar(double valor) {
+        String msg = "Erro na operção tente novamente!";
+        if (valor > 0) {
+            setSaldo(getSaldo() + valor);
+            msg = "Deposito realizado com sucesso";
+        } else {
+            msg = "Somente valores acima de 0";
+        }
+        return msg;
+    }
+
+    public String sacar(double valor) {
+        String msg = "Erro na operção tente novamente!";
+        if (getSaldo() > valor) {
+            setSaldo(getSaldo() - valor);
+            msg = "Saque realizado com sucesso!";
+        } else {
+            msg = "Somente valores acima de 0";
+        }
+
+        return msg;
+    }
+
+    public String extrato(double valor) {
+        return "";
+    }
+
+    public String transferir(double valor) {
+        return "";
+    }
 }

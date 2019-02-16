1cc8bb033c72f40756eda310c7ba73c73e4e15ad
Mateus Oliveira <mateusoli.viana@gmail.com>
2019-02-16 16:00:13 -0200

    FEATURE - create method transferir

--- prjBanco/src/Model/ContaCorrente.java
+++ prjBanco/src/Model/ContaCorrente.java
@@ -1,21 +1,21 @@
 package Model;
 
 public class ContaCorrente extends Pessoa {
-
+    
     private double saldo;
-
+    
     public ContaCorrente(String nome, String sobrenome, int cpf) {
         super(nome, sobrenome, cpf);
     }
-
+    
     public double getSaldo() {
         return saldo;
     }
-
+    
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
-
+    
     public String depositar(double valor) {
         String msg = "Erro na operção tente novamente!";
         if (valor > 0) {
@@ -26,7 +26,7 @@ public class ContaCorrente extends Pessoa {
         }
         return msg;
     }
-
+    
     public String sacar(double valor) {
         String msg = "Erro na operção tente novamente!";
         if (getSaldo() > valor) {
@@ -35,10 +35,10 @@ public class ContaCorrente extends Pessoa {
         } else {
             msg = "Somente valores acima de 0";
         }
-
+        
         return msg;
     }
-
+    
     public String extrato() {
         return "<html>"
                 + "Nome completo: " + getNome() + " " + getSobrenome()
@@ -46,8 +46,16 @@ public class ContaCorrente extends Pessoa {
                 + "<br>Saldo disponível: " + getSaldo()
                 + "</html>";
     }
-
-    public String transferir(double valor) {
-        return "";
+    
+    public String transferir(double valor, ContaCorrente c) {
+        String msg = "Erro na operção tente novamente!";
+        if (getSaldo() > valor) {            
+            c.setSaldo(getSaldo() + valor);
+            setSaldo(getSaldo() - valor);
+            msg = "Transferencia realizada com sucesso!";
+        }else{
+            msg = "Não possui saldo suficiente para realizar a operação. Saldo atual: "+ getSaldo();
+        }
+        return msg;
     }
 }

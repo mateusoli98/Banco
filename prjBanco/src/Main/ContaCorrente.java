b51447af29ffa93c85c7a7402646e54348579044
Mateus Oliveira <mateusoli.viana@gmail.com>
2019-02-16 16:44:29 -0200

    FEATURE - implementação dos metodos

--- prjBanco/src/Main/ContaCorrente.java
+++ prjBanco/src/Main/ContaCorrente.java
@@ -1,62 +1,58 @@
 package Main;
 
-
-
 public class ContaCorrente extends Pessoa {
-    
+
     private double saldo;
-    
+
     public ContaCorrente(String nome, String sobrenome, int cpf) {
         super(nome, sobrenome, cpf);
     }
-    
+
     public double getSaldo() {
         return saldo;
     }
-    
+
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
-    
+
     public String depositar(double valor) {
         String msg = "Erro na operção tente novamente!";
         if (valor > 0) {
             setSaldo(getSaldo() + valor);
-            msg = "Deposito realizado com sucesso";
+            msg = "Deposito realizado com sucesso. Novo saldo: " + getSaldo();
         } else {
             msg = "Somente valores acima de 0";
         }
         return msg;
     }
-    
+
     public String sacar(double valor) {
         String msg = "Erro na operção tente novamente!";
         if (getSaldo() > valor) {
             setSaldo(getSaldo() - valor);
-            msg = "Saque realizado com sucesso!";
+            msg = "Saque realizado com sucesso! Novo saldo: " + getSaldo();
         } else {
-            msg = "Somente valores acima de 0";
+            msg = "Você não possui saldo o suficiente para realizar a operacao. Saldo: " + getSaldo();
         }
-        
+
         return msg;
     }
-    
+
     public String extrato() {
-        return "<html>"
-                + "Nome completo: " + getNome() + " " + getSobrenome()
-                + "<br>CPF: " + getCpf()
-                + "<br>Saldo disponível: " + getSaldo()
-                + "</html>";
+        return "\n\nNome completo: " + getNome() + " " + getSobrenome()
+                + "\nCPF: " + getCpf()
+                + "\nSaldo disponível: " + getSaldo();
     }
-    
+
     public String transferir(double valor, ContaCorrente c) {
         String msg = "Erro na operção tente novamente!";
-        if (getSaldo() > valor) {            
-            c.setSaldo(getSaldo() + valor);
+        if (getSaldo() > valor) {
+            c.setSaldo(c.getSaldo() + valor);
             setSaldo(getSaldo() - valor);
-            msg = "Transferencia realizada com sucesso!";
-        }else{
-            msg = "Não possui saldo suficiente para realizar a operação. Saldo atual: "+ getSaldo();
+            msg = "Transferencia realizada com sucesso! Novo saldo: " + getSaldo();
+        } else {
+            msg = "Não possui saldo suficiente para realizar a operação. Saldo atual: " + getSaldo();
         }
         return msg;
     }
--- prjBanco/src/Main/Main.java
+++ prjBanco/src/Main/Main.java
@@ -1,9 +1,46 @@
 package Main;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
     public static void main(String args[]) {
+
+        ContaCorrente cc1 = new ContaCorrente("Mateus", "Oliveira", 123),
+                      cc2 = new ContaCorrente("João", "de Souza", 456);
         
-      
+        //DEPOSITO NAS CONTAS 1 E 2
+        System.out.println(cc1.depositar(10000));
+        System.out.println(cc2.depositar(5000));
+        
+        //SAQUE NAS CONTAS 1 E 2
+        System.out.println(cc1.sacar(500));
+        System.out.println(cc2.sacar(1000));
+
+        //TRANSFERENCIA DA CONTA 1 PARA 2
+        System.out.println(cc1.transferir(200,cc2));
+        
+        //TRANSFERENCIA DA CONTA 2 PARA 1
+        System.out.println(cc2.transferir(20,cc1));
+        
+        //EXTRATO DAS CONTAS 1 E 2
+        System.out.println(cc1.extrato());
+        System.out.println(cc2.extrato());
+        
+        //EXCESSOES
+        // 1 - DEPOSITAR UM VALOR INVALIDO
+        System.out.println(cc1.depositar(0));
+        System.out.println(cc2.depositar(-10));
+        
+        // 2 - SACAR QUANDO NAO HOUVER SALDO SUFICIENTE
+        System.out.println(cc1.sacar(11000));
+        System.out.println(cc2.sacar(6000));
+        
+        // 3 - TRANSFERENCIA DE UMA QUANTIA MAIOR DO QUE TEM
+        System.out.println(cc1.transferir(12500,cc2));
+        System.out.println(cc2.transferir(7000,cc1));
+        
+        
+        
     }
 }

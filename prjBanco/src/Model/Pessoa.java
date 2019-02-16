1df1ac25d90a7ca3cd3dee6d43b7ed228806c7eb
Mateus Oliveira <mateusoli.viana@gmail.com>
2019-02-16 15:32:12 -0200

    FEATURE - create method construct

--- prjBanco/src/Model/Pessoa.java
+++ prjBanco/src/Model/Pessoa.java
@@ -5,6 +5,13 @@ public class Pessoa {
     private String nome, sobrenome;
     private int cpf;
 
+    public Pessoa(String nome, String sobrenome, int cpf) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.cpf = cpf;
+    }
+
+    
     public String getNome() {
         return nome;
     }

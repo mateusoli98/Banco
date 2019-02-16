c7795392ea01bfc72cbbd2bf700d5dbff3090b73
Mateus Oliveira <mateusoli.viana@gmail.com>
2019-02-16 15:59:41 -0200

    FEATURE - create frame main

--- prjBanco/src/Controller/Main.java
+++ prjBanco/src/Controller/Main.java
@@ -1,8 +1,22 @@
 package Controller;
 
+import View.JFMain;
+
 public class Main {
 
-    public static void main(String[] args) {
-        
+    public static void main(String args[]) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            
+        }
+        java.awt.EventQueue.invokeLater(() -> {
+            new JFMain().setVisible(true);
+        });
     }
 }

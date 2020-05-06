e5a0d93426e39ed00251b7e37d0ffe4bd7c200a0
Mateus Oliveira <mateusoli.viana@gmail.com>
2020-05-06 17:13:34 -0300

    show jFrame bank

--- prjBanco/src/controller/Main.java
+++ prjBanco/src/controller/Main.java
@@ -1,8 +1,23 @@
 package controller;
 
+import view.JFBank;
+
 public class Main {
 
     public static void main(String args[]) {
-
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(JFBank.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+     
+        java.awt.EventQueue.invokeLater(() -> {
+            new JFBank().setVisible(true);
+        });
     }
 }
